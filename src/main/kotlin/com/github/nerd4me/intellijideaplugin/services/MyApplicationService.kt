5976ceffea179bda29ac8142c8320356702c134d
GitHub Action <action@github.com>
2021-10-19 11:53:23 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/nerd4me/intellijideaplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.nerd4me.intellijideaplugin.services
+
+import com.github.nerd4me.intellijideaplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
